84d1599d5d4f97e3f1641aa98ade0e7365fa6afe
shizuuuu <65007855+shizuuuu@users.noreply.github.com>
2021-10-28 14:44:29 +0900

    Added all shops

--- shinjuku_map.js
+++ shinjuku_map.js
@@ -1,17 +1,34 @@
 var map;
 var currentInfoWindow = null;
 
-var place_cafe = [], place_famires = [];
-var markers_cafe = [], markers_famires = []; 
+var place_cafe = [], place_famires = [], place_hamburger = [], place_karaoke = [], place_netcafe = [];
+var markers_cafe = [], markers_famires = [], markers_hamburger = [], markers_karaoke = [], markers_netcafe = []; 
 
-function createData(place, results) {
+function createData(results) {
   for (let i=0; i<results.length; i++) {
     x = results[i].geometry.location;
     y = results[i].name;
     z = results[i].price_level;
     x["name"] = y;
     x["price"] = z;
-    place.push(x);
+
+    switch(results[i].shop) {
+      case "cafe":
+        place_cafe.push(x);
+        break;
+      case "famires":
+        place_famires.push(x);
+        break;
+      case "hamburger":
+        place_hamburger.push(x);
+        break;
+      case "karaoke":
+        place_karaoke.push(x);
+        break;
+      case "netcafe":
+        place_netcafe.push(x);
+        break;
+    }
   } 
   /*
   price:
@@ -59,6 +76,24 @@ function famires(map) {
   }
 }
 
+function hamburger(map) {
+  for (var i=0; i<place_hamburger.length; i++) {
+    createMarker(map, i, place_hamburger, markers_hamburger);
+  }
+}
+
+function karaoke(map) {
+  for (var i=0; i<place_karaoke.length; i++) {
+    createMarker(map, i, place_karaoke, markers_karaoke);
+  }
+}
+
+function netcafe(map) {
+  for (var i=0; i<place_netcafe.length; i++) {
+    createMarker(map, i, place_netcafe, markers_netcafe);
+  }
+}
+
 function initMap() {
   var target = document.getElementById('map');  
   var latlng = { lat: 35.69092, lng: 139.7002579 };
@@ -66,8 +101,10 @@ function initMap() {
     center: latlng,
     zoom: 16
   });
-  createData(place_cafe, shinjuku_cafe.results);
-  createData(place_famires, shinjuku_famires.results);
+  createData(shinjuku_data.results);
   cafe(map);
   famires(map);
+  hamburger(map); 
+  karaoke(map);
+  netcafe(map);
 }
