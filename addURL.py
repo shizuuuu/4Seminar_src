e0fc0bab059c4bcb3696c174b1c05f0af72e4c92
shizuuuu <65007855+shizuuuu@users.noreply.github.com>
2021-12-15 23:28:40 +0900

    Update addURL.py

--- addURL.py
+++ addURL.py
@@ -1,11 +1,11 @@
 import json
 
 with open('***.json','r',encoding="utf-8") as f_in, open('***.js','w',encoding='utf-8') as f_out:
-    jsn_shinjuku = json.load(f_in)
-    f_out.write("test_data = ")   
+    jsn_data = json.load(f_in)
+    f_out.write("***_data = ")   
     dct = {}
     lst = []
-    for x,y in jsn_shinjuku.items():
+    for x,y in jsn_data.items():
         dct[x] = y
         if x == 'results':
             tmp = {}
